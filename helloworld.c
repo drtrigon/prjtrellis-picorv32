e0d02d2e4d2249f2e31ad862eb40bd25148c300d
drtrigon <dr.trigon@surfeu.ch>
2019-01-18 23:11:49 +0100

    improvement; * start testing with helloworld.c also

--- helloworld.c
+++ helloworld.c
@@ -5,6 +5,10 @@
 #define reg_uart_clkdiv (*(volatile uint32_t*)0x02000004)
 #define reg_uart_data (*(volatile uint32_t*)0x02000008)
 
+//#define F_CPU 25000000 // Hz
+#define F_CPU 12000000 // Hz
+#define BAUD_RATE 115200 // bit/s
+
 int get_available()
 {
   return reg_uart_data & 0x100;
@@ -35,17 +39,18 @@ void delay() {
 
 int main() {
     char *get = "\n";
-    reg_uart_clkdiv = 416/2;
+    reg_uart_clkdiv = F_CPU/BAUD_RATE; // sets baudrate (115200 @ 25 MHz)
+//    reg_uart_clkdiv = 103; // sets baudrate (115200 @ 25 MHz)
     while (1) {
         LED = 0xFF;
-        print("hello world\n");
-        delay();
-        if(get_available())
-        {
-          get[0] = getchar();
-          print(get);
-        }
+//        print("hello world\n");
+//        delay();
+//        if(get_available())
+//        {
+//          get[0] = getchar();
+//          print(get);
+//        }
         LED = 0x00;
-        delay();
+//        delay();
     }
 }
